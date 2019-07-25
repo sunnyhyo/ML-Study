7961a06e3587b80975453f240c10e8bda8afb67a
Hyosun, Park <sunnyhyo77@gmail.com>
2019-07-25 18:37:45 +0900

    정리

---
+++ ml-100k/chdir1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 13 04:27:01 2018
+
+@author: HS
+"""
+
+#%%
+import os
+
+print(os.getcwd())
+os.chdir("c:/Users/HS/Documents/GitHub/ml-study/ml-100k")
